dc9a1e02476444823fa42808f9dc4a2459ad4257
robalaban <rob.balaban@gmail.com>
2018-08-26 23:08:59 +0100

    Refactored API calls to prevent duplicates in database, and check for API updates to append

--- app/nass_crop_conditions_kansas.py
+++ app/nass_crop_conditions_kansas.py
@@ -3,37 +3,67 @@ import requests
 from __main__ import db
 from models import crop_conditions_kansas
 
-# Building the query dict
-QUERYDICT = {
-    "api_key": os.getenv('NASS_API_KEY', ''),
-    "crop": "CORN",
-    "year": 2012, 
-    "state": "KS",
-    "frequency": "WEEKLY",
-    "condition": "CORN - CONDITION, MEASURED IN PCT EXCELLENT"
-}
+def build_request_query(condition, timeframe):
+    return {
+        "api_key": os.getenv('NASS_API_KEY', ''),
+        "crop": "CORN",
+        "condition": condition,
+        "timeframe": timeframe,
+        "state": "KS",
+        "frequency": "WEEKLY",
+        "description": "CORN - CONDITION, MEASURED IN PCT EXCELLENT"
+    }
 
-REQUEST_URL = (
+def format_request_url(query_dict):    
+    return (
         "https://quickstats.nass.usda.gov/api/api_GET/?"
         "key={api_key}&"
         "commodity_desc={crop}&"
-        "year__GE={year}&"
+        "{condition}={timeframe}&"
         "state_alpha={state}&"
         "freq_desc={frequency}&"
-        "short_desc={condition}&"
+        "short_desc={description}&"
         "format=JSON"
-    ).format(**QUERYDICT)
+    ).format(**query_dict)
 
 def fetch_data_and_send_to_db():
-    request = requests.get(REQUEST_URL)
+    """This function checks wethere or not the database is up to date
+    calls the NASS api by constructing the necesary queries
+
+    :return: param[object]: request
+    """
+
+    # Grabbing last record from the database,
+    record = crop_conditions_kansas.query.order_by(
+                crop_conditions_kansas.week_ending.desc()
+            ).first()
+
+    if record is None:
+        # Makes request if no record is found
+        query = build_request_query('year__GE', '2012')
+        request = requests.get(format_request_url(query))
+
+        send_to_db(request)
+    else:
+        # If record is found checks to add 
+        query = build_request_query('week_ending__GT', record.week_ending)
+        request = requests.get(format_request_url(query))
+        send_to_db(request)
+    
+
+
+def send_to_db(request):
+    """This function checks the reqeust code, normalizez the data
+    and inserts into the database
+
+    :param param[object] request: The request object from the API
+    :return: None
+    """
+
     if request.status_code == 200:
         db_rows = request.json()['data']
-    
-    """ Some data manipulation before sending to db
-        removed CV (%) from object and normalized value
-        field
-    """
-    if db_rows:
+        
+        # Data manipulation, removed CV (%) from object
         for idx in range(len(db_rows)):
             try:
                 db_rows[idx]["value"] = db_rows[idx].pop("Value")
@@ -41,12 +71,5 @@ def fetch_data_and_send_to_db():
             except KeyError:
                 pass
 
-def send_to_db(db_rows):
-    """This function abstracts/mocks the database connection
-
-    :param list[dict] db_rows: The data for the db
-    :return: None
-    """
-
-    db.session.bulk_insert_mappings(crop_conditions_kansas, db_rows)   
-    db.session.commit()
+        db.session.bulk_insert_mappings(crop_conditions_kansas, db_rows)   
+        db.session.commit()
