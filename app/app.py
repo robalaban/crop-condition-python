28743377b4d8f7752234ff0386d7bdf4b6f3635b
robalaban <rob.balaban@gmail.com>
2018-08-26 19:18:00 +0100

    Connect to db + starting crop_conditions script

--- app/app.py
+++ app/app.py
@@ -17,5 +17,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secretkey'
 
+# Importing the database after the app has been created
+from models import db
+from nass_crop_conditions_kansas import fetch_data_and_send_to_db
+
 if __name__ == '__main__':
+    db_flag = False
+    while db_flag == False:
+        try:
+            db.create_all()
+        except:
+            time.sleep(2)
+        else:
+            db_flag = True
     app.run(debug=True, host='0.0.0.0')
